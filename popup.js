6d1d306c144aaae3a3946e3840b2f44070833352
ryn5 <ryanmtan5@gmail.com>
2022-01-26 20:24:18 -0800

    Implement button links

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
-// UPass last updated: date time
-// changes when load upass button is clicked
-// add links to the buttons
+const homeButton = document.querySelector('#home-btn');
+
+homeButton.addEventListener('click', () => {
+    window.open('https://www.compasscard.ca/');
+});
+
+const checkButton = document.querySelector('#check-btn');
+
+checkButton.addEventListener('click', () => {
+    window.open('https://www.compasscard.ca/ManageCards');
+});
+
+const upassButton = document.querySelector('#upass-btn');
+
+upassButton.addEventListener('click', () => {
+    // window.open('https://upassbc.translink.ca/');
