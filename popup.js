29f643092335c259e9df6d3a09106488905905ac
ryn5 <ryanmtan5@gmail.com>
2022-02-04 01:15:05 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,39 +1,50 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+function setup() {
+    if (localStorage['updated-text']) {
+        document.getElementById('updated-text').innerHTML = localStorage['updated-text'];
+    }
+    
+    // const date = Date() something;
+    // if (date.getDay() > 16 && localStorage['month'] === date.getMonth()) {
+    //     sendmessage
+    // }
 
-async function helloSelenium() {
-    let driver = await new Builder().forBrowser('chrome').build();
 
-    await driver.get('https://selenium.dev');
+    const homeButton = document.querySelector('#home-btn');
 
-    await driver.quit();
+    homeButton.addEventListener('click', () => {
+        window.open('https://www.compasscard.ca/');
+
+    });
+
+
+    const checkButton = document.querySelector('#check-btn');
+
+    checkButton.addEventListener('click', () => {
+        chrome.runtime.sendMessage({ data: "notif", hello: 'yes' }, function (response) {
+            console.log("Response received in popup.js");
+        });
+
+    });
+
+
+    const upassButton = document.querySelector('#upass-btn');
+
+    upassButton.addEventListener('click', () => {
+        window.open('https://upassbc.translink.ca/');
+        updateText();
+    });
 }
 
-helloSelenium();
-
-if (localStorage['updated-text']) {
-    document.getElementById('updated-text').innerHTML = localStorage['updated-text']; 
-}
-
-const homeButton = document.querySelector('#home-btn');
-
-homeButton.addEventListener('click', () => {
-    window.open('https://www.compasscard.ca/');
-});
-
-const checkButton = document.querySelector('#check-btn');
-
-checkButton.addEventListener('click', () => {
-    window.open('https://www.compasscard.ca/ManageCards');
-});
-
-const upassButton = document.querySelector('#upass-btn');
-
-upassButton.addEventListener('click', () => {
-    window.open('https://upassbc.translink.ca/');
+function updateText() {
     const date = new Date();
     const options = { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
     const dateFormat = date.toLocaleString('en-US', options);
     const str = 'Last updated: ' + dateFormat;
     document.getElementById('updated-text').innerHTML = str;
     localStorage['updated-text'] = str;
+    localStorage['month'] = date.getMonth();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    setup();
 });
