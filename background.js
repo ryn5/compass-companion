59777f45c07384297310723fbfff872bd70b1351
ryn5 <ryanmtan5@gmail.com>
2022-02-11 20:03:00 -0800

    Add webpack and node

--- background.js
+++ background.js
@@ -14,3 +14,16 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
         chrome.notifications.create('reminder-notif', opt, function (id) {});
     }
 });
+
+chrome.runtime.onStartup.addListener(function() {
+    reminder();
+});
+
+function reminder() {
+    const date = new Date();
+    // if (date.getDay() > 5 && localStorage['month'] === date.getMonth()) { // try with today's date and reopen chrome
+        chrome.runtime.sendMessage({msg: 'notif'}, function (response) { // try sending msg as plain string
+            console.log(response);
+        });
+    // }
+}
