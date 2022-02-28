5fd77b3a8e0017fa16bcfde3b044d1f3e3dfb729
ryn5 <ryanmtan5@gmail.com>
2022-02-27 17:33:46 -0800

    Formatting

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 require('dotenv').config();
 
 const cors = require('cors');
